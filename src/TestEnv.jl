40cd14ebd0787839a91b54cc8b751a3ae39cc7e7
Lyndon White <lyndon.white@invenialabs.co.uk>
2021-08-01 22:18:18 +0100

    Simplify activate_set by copying the manifest out of the sandbox and using it after

--- src/TestEnv.jl
+++ src/TestEnv.jl
@@ -1,14 +1,11 @@
 module TestEnv
 using Pkg
 using Pkg: PackageSpec
-using Pkg.Types: Context, ensure_resolved, is_project_uuid, write_env
-using Pkg.Types: Types, projectfile_path, manifestfile_path, SHA1
+using Pkg.Types: Context, ensure_resolved, is_project_uuid, SHA1
 using Pkg.Operations: manifest_info, manifest_resolve!, project_deps_resolve!
 using Pkg.Operations: project_rel_path, project_resolve!
 using Pkg.Operations: with_dependencies_loadable_at_toplevel, find_installed
-using Pkg.Operations: get_deps, collect_target_deps!, add_or_develop
-using Pkg.Operations: resolve_versions!, apply_versions, build_versions, prune_manifest
-using UUIDs
+
 
 include("common.jl")
 include("activate_do.jl")
