aa9174eba20e96d181654924fc857e4716d45eb4
WT <wt0881@my.bristol.ac.uk>
2021-08-10 14:04:00 +0100

    use is_fixed from Pkg

--- src/TestEnv.jl
+++ src/TestEnv.jl
@@ -3,7 +3,7 @@ using Pkg
 using Pkg: PackageSpec
 using Pkg.Types: Context, ensure_resolved, is_project_uuid, write_env, is_stdlib
 using Pkg.Types: Types, projectfile_path, manifestfile_path
-using Pkg.Operations: manifest_info, manifest_resolve!, project_deps_resolve!
+using Pkg.Operations: manifest_info, manifest_resolve!, project_deps_resolve!, isfixed
 using Pkg.Operations: project_rel_path, project_resolve!
 using Pkg.Operations: sandbox, source_path, sandbox_preserve, abspath!
 using Pkg.Operations: gen_target_project, update_package_test!
