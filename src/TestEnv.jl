201faced401f3206b4fc34ec31202c77e55af854
WT <wt0881@my.bristol.ac.uk>
2021-08-10 15:18:17 +0100

    Fix typo

--- src/TestEnv.jl
+++ src/TestEnv.jl
@@ -6,7 +6,7 @@ using Pkg.Types: Types, projectfile_path, manifestfile_path
 using Pkg.Operations: manifest_info, manifest_resolve!, project_deps_resolve!
 using Pkg.Operations: project_rel_path, project_resolve!
 using Pkg.Operations: sandbox, source_path, sandbox_preserve, abspath!
-using Pkg.Operations: gen_target_project, update_package_test!, is_fixed
+using Pkg.Operations: gen_target_project, update_package_test!, isfixed
 
 include("common.jl")
 include("activate_do.jl")
