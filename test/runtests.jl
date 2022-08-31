b61a930d93453e9cbb7b956dce38ab78be3e94a1
Mal Miller <59854849+mmiller-max@users.noreply.github.com>
2022-08-31 15:11:24 +0100

    Add a test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using TestEnv
 using Test
 
 @testset "TestEnv.jl" begin
-    include("activate_do.jl")
-    include("activate_set.jl")
+    # include("activate_do.jl")
+    # include("activate_set.jl")
+    include("common.jl")
 end
